3ed4c4cb9d00156837b414f879defdd14837e665
Andrian <andrian_b2@mail.ru>
2017-03-30 13:37:57 +0300

    initial commit

---
+++ src/main/java/org/andrianb/SuntehnicApplication.java
@@ -0,0 +1,12 @@
+package org.andrianb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuntehnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuntehnicApplication.class, args);
+	}
+}
