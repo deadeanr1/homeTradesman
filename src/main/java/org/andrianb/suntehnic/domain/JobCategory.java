b2e99cc5dca73e60292695a171a96d7a1761131f
Andrian <andrian_b2@mail.ru>
2017-04-11 20:28:15 +0300

    added jobcategory and jobsubcategory

--- src/main/java/org/andrianb/suntehnic/domain/JobCategory.java
+++ src/main/java/org/andrianb/suntehnic/domain/JobCategory.java
@@ -1,7 +1,69 @@
 package org.andrianb.suntehnic.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Created by User on 4/11/2017.
  */
-public class JobCategory {
+
+@Entity
+@Table(name = JobCategory.TABLE_NAME)
+public class JobCategory implements Serializable{
+    public static final String TABLE_NAME = "job_category";
+
+    public static final String COLUMN_ID = "solution_type_id";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_DESCIPTION = "description";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = COLUMN_ID)
+    @JsonIgnore
+    private Long id;
+
+    @Column(name = COLUMN_TITLE)
+    private String title;
+
+    @Column(name = COLUMN_DESCIPTION)
+    private String description;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = JobCategoryType.COLUMN_PARENT, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<JobCategoryType> taskTypes;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public List<JobCategoryType> getTaskTypes()
+    {
+        return taskTypes;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
 }
+
---
+++ src/main/java/org/andrianb/suntehnic/domain/JobCategoryType.java
@@ -0,0 +1,63 @@
+package org.andrianb.suntehnic.domain;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by User on 4/11/2017.
+ */
+
+@Entity
+@Table(name = "job_category_type")
+public class JobCategoryType implements Serializable {
+
+    public static final String COLUMN_ID = "solution_task_type_id";
+    public static final String COLUMN_PARENT = "parent";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = COLUMN_ID)
+    //@JsonIgnore
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = JobCategory.COLUMN_ID)
+    //  @JsonIgnore
+    private JobCategory parent;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public JobCategory getParent() {
+        return parent;
+    }
+
+    public void setOwner(JobCategory parent) {
+        this.parent = parent;
+    }
+
+
+}
+
